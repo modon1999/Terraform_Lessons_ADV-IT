97097214507d3a1543b8d92150ee0d91237db5f6
modon1999 <modon19@yandex.ru>
2022-03-06 04:56:08 -0800

    Lesson with Dependencies depends_on

---
+++ Lesson-8/main.tf
@@ -0,0 +1,79 @@
+#-------------------------------------------------------
+# Build WebServer during Bootstrap
+#-------------------------------------------------------
+
+provider "aws" {
+}
+
+resource "aws_instance" "my_server_web" {
+  ami                    = "ami-0eb7496c2e0403237"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+
+  tags = {
+    Name    = "Server-Web"
+    Owner   = "Nikita Grigorev"
+    Project = "Terraform Lessons"
+  }
+  depends_on = [aws_instance.my_server_db, aws_instance.my_server_app]
+}
+
+resource "aws_instance" "my_server_app" {
+  ami                    = "ami-0eb7496c2e0403237"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+
+  tags = {
+    Name    = "Application-Server"
+    Owner   = "Nikita Grigorev"
+    Project = "Terraform Lessons"
+  }
+
+  depends_on = [aws_instance.my_server_db]
+}
+
+resource "aws_instance" "my_server_db" {
+  ami                    = "ami-0eb7496c2e0403237"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+
+  tags = {
+    Name    = "Database-Server"
+    Owner   = "Nikita Grigorev"
+    Project = "Terraform Lessons"
+  }
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "My First SecurityGroup by Terraform"
+
+  dynamic "ingress" {
+    for_each = ["80", "443", "1541", "9999", "7777"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "my_webserver_security_group"
+    Owner   = "Nikita Grigorev"
+    Project = "Terraform Lessons"
+  }
+}
