0e2868c15af7d0587bd951efb388ab923929866e
modon1999 <modon19@yandex.ru>
2022-03-06 04:55:10 -0800

    Lesson with LifeCycle and EIP

---
+++ Lesson-6/WebServer.tf
@@ -0,0 +1,65 @@
+#-------------------------------------------------------
+# Build WebServer during Bootstrap
+#-------------------------------------------------------
+
+provider "aws" {
+}
+
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.my_webserver.id
+
+}
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-0eb7496c2e0403237"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Nikita",
+    l_name = "Grigorev",
+    names  = ["Vasya", "John", "Vika", "Tatyana", "Test"]
+  })
+
+  tags = {
+    Name    = "my_webserver"
+    Owner   = "Nikita Grigorev"
+    Project = "Terraform Lessons"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "My First SecurityGroup by Terraform"
+
+  dynamic "ingress" {
+    for_each = ["80", "443", "1541", "9999", "7777"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "my_webserver_security_group"
+    Owner   = "Nikita Grigorev"
+    Project = "Terraform Lessons"
+  }
+}
